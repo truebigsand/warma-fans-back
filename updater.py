65385256119686cd398e2d317d8f28322351ee66
truebigsand <3244346642@qq.com>
2021-05-03 14:49:16 +0800

    upd

--- updater.py
+++ updater.py
@@ -15,10 +15,10 @@ def update():
         tmp = json.loads(requests.get(f'https://api.bilibili.com/x/space/arc/search?mid={uid}&ps=30&pn={pageNum}&jsonp=jsonp').text)
         for video in tmp['data']['list']['vlist']:
             db.execute('INSERT INTO videos VALUES({},{},{},"{}","{}","{}","{}",{},{},"{}",{},"{}",{})'.format(video['comment'],video['typeid'],video['play'],video['pic'],video['description'],video['title'],video['author'] ,video['mid'],video['created'],video['length'],video['aid'],video['bvid'],video['is_union_video']))
-            print('INSERT INTO videos VALUES({},{},{},"{}","{}","{}","{}",{},{},"{}",{},"{}",{})'.format(video['comment'],video['typeid'],video['play'],video['pic'],video['description'],video['title'],video['author'] ,video['mid'],video['created'],video['length'],video['aid'],video['bvid'],video['is_union_video']))
+            #print('INSERT INTO videos VALUES({},{},{},"{}","{}","{}","{}",{},{},"{}",{},"{}",{})'.format(video['comment'],video['typeid'],video['play'],video['pic'],video['description'],video['title'],video['author'] ,video['mid'],video['created'],video['length'],video['aid'],video['bvid'],video['is_union_video']))
         limit_now += 30
         pageNum += 1
-    print(db.query('SELECT * FROM videos'))
+    #sprint(db.query('SELECT * FROM videos'))
     #except:
     #    return '{"msg":"failed"}'
     return '{"msg":"success"}'
